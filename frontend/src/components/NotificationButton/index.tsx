c98d8db44381a3f257ad1efea37d81a1c0c88756
unknown <nathan.renovato300@gmail.com>
2022-11-18 12:36:33 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -2,6 +2,7 @@ import Icon from "../../assets/img/notification-icon.svg";
 import { BASE_URL } from "../../utils/request";
 import "./styles.css";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 type Props = {
   saleId: number;
@@ -9,7 +10,7 @@ type Props = {
 
 function handleClick(id: number) {
   axios(`${BASE_URL}/sales/${id}/notification`).then((response) => {
-    console.log("Sucesso");
+    toast.info("SMS enviado com sucesso");
   });
 }
 
