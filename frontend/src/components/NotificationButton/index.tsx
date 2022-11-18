2ebebf6bcfd1d3f7e10159a9388898bab30b5326
unknown <nathan.renovato300@gmail.com>
2022-11-18 12:30:21 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,21 @@
 import Icon from "../../assets/img/notification-icon.svg";
+import { BASE_URL } from "../../utils/request";
 import "./styles.css";
+import axios from "axios";
 
-function NotificationButton() {
+type Props = {
+  saleId: number;
+};
+
+function handleClick(id: number) {
+  axios(`${BASE_URL}/sales/${id}/notification`).then((response) => {
+    console.log("Sucesso");
+  });
+}
+
+function NotificationButton({ saleId }: Props) {
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
       <img src={Icon} alt="Notificar" />
     </div>
   );
